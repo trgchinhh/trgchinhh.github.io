9c17d661d923f94fd2602dc6dda9eb506117f3ec
trgchinhh <chinhcuber@gmail.com>
2026-09-24 20:50:32 +0700

    Sửa lại thông tin

--- data.js
+++ data.js
@@ -14,37 +14,39 @@ const INFO = [
     id: "b1",
     content: `
 <h2>Giới thiệu</h2>
-<p>Mình là Trường Chinh, sinh viên năm 3 trường Đại học Công nghệ Sài Gòn (STU)</p>
+<p>Mình là Trường Chinh, sinh viên năm 3 ngành công nghệ thông tin trường Đại học Công nghệ Sài Gòn (STU)</p>
 
 <h2>Thành tích</h2>
 <div id="achievementList"></div>
 
 <h2>Sở thích</h2>
 <p>Mình thích thiết kế và xây dựng phần mềm mã nguồn mở, 
-đặc biệt quan tâm đến công nghệ blockchain, thuật toán mật mã và các tiêu chuẩn an ninh mạng như NIST. 
-Bên cạnh đó mình còn thích làm việc với các ứng dụng cấp thấp, dựa trên giao diện dòng lệnh và lập trình hệ thống
+đặc biệt quan tâm đến công nghệ blockchain, các thuật toán mật mã hiện đại và các tiêu chuẩn an ninh mạng như NIST. 
+Bên cạnh đó mình còn thích phát triển các chương trình và công cụ bằng ngôn ngữ cấp thấp như C++, dựa trên giao diện dòng lệnh và lập trình hệ thống
 </p>
 
 <h2>Mục tiêu</h2>
-<p>Trở thành kỹ sư backend Web3/Blockchain trong tương lai</p>
+<p>Trở thành kỹ sư Backend Web3/Blockchain trong tương lai</p>
 
 <h2>Kỹ năng</h2>
 <ul>
-  <li><strong>Ngôn ngữ lập trình: </strong>C++ / Python / C# / Java cơ bản</li>
+  <li><strong>Ngôn ngữ lập trình: </strong>C++ / Python / C# cơ bản</li>
   <li><strong>Công nghệ: </strong>Git / Github</li>
   <li><strong>Web: </strong>HTML / CSS / JavaScript cơ bản</li>
-  <li><strong>Framework: </strong>ExpressJS cơ bản</li>
+  <li><strong>Framework: </strong>ExpressJS / ASP.NET cơ bản</li>
   <li><strong>Linux: </strong>Ubuntu / WSL</li>    
-  <li><strong>Khác: </strong>SSH / BatchScript cơ bản</li>
+  <li><strong>Khác: </strong>SSH / Batchscript cơ bản</li>
+  <li><strong>Code editor: </strong>Sublime Text + Windows Terminal</li>
 </ul>
 
 <h2>Kinh nghiệm</h2>
 <ul>
-  <li>Tạo module và build library bằng Python</li>
-  <li>Tạo bot Telegram lấy thông tin bằng API hoặc bot game</li>
-  <li>Mô phỏng Sicbo game CLI (custom TUI) bằng C++</li>
-  <li>Mô phỏng blockchain và cách validators (P2P) vận hành</li>
-  <li>Viết mô phỏng CLI cho các thuật toán thực tế</li>
+  <li>Xây dựng và phát hành thư viện mã nguồn mở bằng Python</li>
+  <li>Phát triển các bot telegram, thu thập dữ liệu qua API, bot games</li>
+  <li>Phát triển mini games sicbo CLI với giao diện custom TUI bằng C++</li>
+  <li>Mô phỏng Mini Blockchain và cơ chế vận hành của các validator (P2P) bằng C++</li>
+  <li>Viết mô phỏng CLI cho các thuật toán thực tế như Scan</li>
+  <li>Viết mô phỏng CLI các thuật toán mã hóa cơ bản (Caesar, AES, RSA)</li>
 </ul>
     `
   }
